6f271c24dc0435abcc0fad7bedb6bff07b185ea5
7minutes-7 <konho3@naver.com>
2020-09-13 16:02:45 +0900

    열쇠 생성

--- GitRoomEscape.cpp
+++ GitRoomEscape.cpp
@@ -6,4 +6,13 @@ int main()
 	auto scene1 = Scene::create("룸1", "RoomEscape_Images/배경-1.png");
 	auto scene2 = Scene::create("룸2", "RoomEscape_Images/배경-2.png");
 
+	//열쇠를 만든다.
+	auto key = Object::create("RoomEscape_Images/열쇠.png", scene1, 1128, 133);
+	key->setScale(0.2f);
+
+	key->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		key->pick();
+		return true;
+		});
+
 }
