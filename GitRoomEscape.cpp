07882966e1ad0ffddec9d82355372545fbebd209
7minutes-7 <konho3@naver.com>
2020-09-13 16:02:06 +0900

    장면 생성

--- GitRoomEscape.cpp
+++ GitRoomEscape.cpp
@@ -2,5 +2,8 @@
 using namespace bangtal;
 int main()
 {
+	//장면을 생성한다.
+	auto scene1 = Scene::create("룸1", "RoomEscape_Images/배경-1.png");
+	auto scene2 = Scene::create("룸2", "RoomEscape_Images/배경-2.png");
 
 }
