12fbab653b8975f4d8c78828c40998c6f82e2813
7minutes-7 <konho3@naver.com>
2020-09-13 16:03:38 +0900

    박스 생성

--- GitRoomEscape.cpp
+++ GitRoomEscape.cpp
@@ -15,4 +15,28 @@ int main()
 		return true;
 		});
 
+	//박스를 만든다.
+	auto box1_moved = false;
+	auto box2_moved = false;
+	auto box3_moved = false;
+	auto box4_moved = false;
+	auto box5_moved = false;
+	auto box6_moved = false;
+	auto box7_moved = false;
+
+	auto box1 = Object::create("RoomEscape_Images/box.png", scene1, 330, 170);
+	box1->setScale(0.01f);
+	auto box2 = Object::create("RoomEscape_Images/box.png", scene1, 550, 30);
+	box2->setScale(0.01f);
+	auto box3 = Object::create("RoomEscape_Images/box.png", scene1, 1100, 130);
+	box3->setScale(0.01f);
+	auto box4 = Object::create("RoomEscape_Images/box.png", scene1, 790, 90);
+	box4->setScale(0.01f);
+	auto box5 = Object::create("RoomEscape_Images/box.png", scene1, 600, 190);
+	box5->setScale(0.01f);
+	auto box6 = Object::create("RoomEscape_Images/box.png", scene1, 200, 15);
+	box6->setScale(0.01f);
+	auto box7 = Object::create("RoomEscape_Images/box.png", scene1, 130, 190);
+	box7->setScale(0.01f);
+
 }
